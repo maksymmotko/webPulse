8a1d6972aeb8f8577b6d4f1ccf53025b1f719428
Maksym <maksym.motko@gmail.com>
2021-08-02 18:20:38 +0200

    lesson_animate

--- src/js/script.js
+++ src/js/script.js
@@ -93,4 +93,38 @@ $(document).ready(function(){
       valideForms('#consultation form');
       valideForms('#order form');
 
+      $('form').submit(function(e) {
+          e.preventDefault();
+          $.ajax({
+              type: "POST",
+              url: "../../mailer/smart.php",
+              data: $(this).serialize()
+          }).done(function() {
+              $(this).find("input").val("");
+              $('#consultation, #order').fadeOut();
+              $('.overlay, #thanks').fadeIn('slow');
+
+              $('form').trigger('reset');
+          });
+          return false;
+      });
+
+      //Smooth scroll and pageup
+
+      $(window).scroll(function() {
+          if ($(this).scrollTop() > 1600) {
+              $('.pageup').fadeIn();
+          } else {
+              $('.pageup').fadeOut();
+          }
+      });
+
+      $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+
   });
